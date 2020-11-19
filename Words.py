59218b716a67f4bc49278f5c42297b1fbf2fc5d0
ewanina <nlewandowska@outlook.com>
2020-11-19 09:56:13 +0100

    Praca Domowa nr 2

---
+++ Words.py
@@ -0,0 +1,37 @@
+file = 'LoremIpsum.txt'
+with open(file, 'a') as outfile:
+    outfile.write(
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nibh augue, suscipit a, scelerisque sed, lacinia in, mi. Cras vel lorem. Etiam pellentesque aliquet tellus. Phasellus pharetra nulla ac diam. Quisque semper justo at risus. Donec venenatis, turpis vel hendrerit interdum, dui ligula ultricies purus, sed posuere libero dui id orci. Nam congue, pede vitae dapibus aliquet, elit magna vulputate arcu, vel tempus metus leo non est. Etiam sit amet lectus quis est congue mollis. Phasellus congue lacus eget neque. Phasellus ornare, ante vitae consectetuer consequat, purus sapien ultricies dolor, et mollis pede metus eget nisi. Praesent sodales velit quis augue. Cras suscipit, urna at aliquam rhoncus, urna quam viverra nisi, in interdum massa nibh nec erat.')
+
+
+# Open the file in read mode
+text = open(file, "r")
+
+# Create an empty dictionary
+d = dict()
+
+# Loop through each line of the file
+for line in text:
+    # Remove the leading spaces and newline character
+    line = line.strip()
+
+    # Convert the characters in line to
+    # lowercase to avoid case mismatch
+    line = line.lower()
+
+    # Split the line into words
+    words = line.split(" ")
+
+    # Iterate over each word in line
+    for word in words:
+        # Check if the word is already in dictionary
+        if word in d:
+            # Increment count of word by 1
+            d[word] = d[word] + 1
+        else:
+            # Add the word to dictionary with count 1
+            d[word] = 1
+
+# Print the contents of dictionary
+for key in list(d.keys()):
+    print(key, ":", d[key])
